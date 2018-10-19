1e65214595fe95a8db85cd3c96d85a9bdf152429
Derek Ayres <wunderlad@Hotmail.com>
2018-10-19 07:42:15 -0400

    Revisions 2 FINAL

--- js/app.js
+++ js/app.js
@@ -18,82 +18,22 @@ for (var i = 0; i < kitties.length; i++) {
   var kittyNameHolderContent = document.createTextNode(kitty.name);
   var image = document.createElement("img");
   var counter = document.createElement("div");
-  var counterCount = document.createTextNode("Clicky click counter: " + kitty.clickCount);
-/*
-I CANT SEEM TO GET THE CLICKER WORKING.
-  counter.addEventListener('click', (function(numCopy) {
+  var counterCount = document.createElement("span");
+  counterCount.innerHTML = ("Clicky click counter: " + kitty.clickCount);
+  kitty.counterCount = counterCount;
+
+  image.addEventListener('click', (function(kittyCopy) {
     return function() {
-    clickCount++;
-  })(clickCount);
-*/
+      kittyCopy.clickCount++;
+      kittyCopy.counterCount.innerHTML = "Clicky click counter: " + kittyCopy.clickCount;
+      console.log (kittyCopy);
+    };
+  })(kitty));
+
   kittyNameHolder.appendChild(kittyNameHolderContent);
   image.src = kitty.url;
   counter.appendChild(counterCount);
   master.appendChild(kittyNameHolder);
   master.appendChild(image);
-  master.appendChild(counter)
+  master.appendChild(counter);
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-CODEPEN.COM
-elem.addEventListener('click', function(){
-  catcount++;
-  document.getElementById('catcount').innerHTML = "Clicky Click Counter:  " + catcount;
-  //the element has been clicked... do stuff here
-  console.log ('die kitty, die');
-}, false);
-
-
-
-// clear the screen for testing
-document.body.innerHTML = '';
-document.body.style.background="white";
-
-var nums = [1,2,3];
-
-// Let's loop over the numbers in our array
-for (var i = 0; i < nums.length; i++) {
-
-    // This is the number we're on...
-    var num = nums[i];
-
-    // We're creating a DOM element for the number
-    var elem = document.createElement('div');
-    elem.textContent = num;
-
-    // ... and when we click, alert the value of `num`
-    elem.addEventListener('click', function() {
-        alert(num);
-    });
-
-    // finally, let's add this element to the document
-    document.body.appendChild(elem);
-};
-
-
-
-/*
-var elem = document.getElementById('kitty');
-elem.addEventListener('click', function(){
-
-  console.log ('die kitty, die');
-}, false);
-
-
-let catcount = 0;
-
-function onClick(){
-  cat-count++;
-  document.getElementById('catcount').innerHTML = "Clicky Click Counter:  " + catcount;
-}
-*/
